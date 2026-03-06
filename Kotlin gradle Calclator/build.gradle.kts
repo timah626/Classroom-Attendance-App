a0e51247fc5cf2b7f25a06dd0b06152428d4dece
timah626 <timahchelsie@gmail.com>
2026-03-06 22:08:54 +0100

    Configure Kotlin and dependencies in build.gradle.kts

--- Kotlin gradle Calclator/build.gradle.kts
+++ Kotlin gradle Calclator/build.gradle.kts
@@ -1,1 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
 
+group = "com.attendance"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Kotlin standard library
+    implementation(kotlin("stdlib"))
+    
+    // Apache POI for Excel file handling
+    implementation("org.apache.poi:poi:5.2.3")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    
+    // Testing
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
