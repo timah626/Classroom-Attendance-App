8d13a49412a876a0738fde6b41c2f737207f9969
Randzyj <kuwanrandy@gmail.com>
2026-03-07 00:41:20 +0100

    Milestone 2: Upgraded Exercise 1 logic with Lambdas and OOP

--- exercise_01/test/exercise_01_test.dart
+++ exercise_01/test/exercise_01_test.dart
@@ -1,8 +1,6 @@
-import 'package:exercise_01/exercise_01.dart';
+import 'package:exercise_01/main.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+
 }
