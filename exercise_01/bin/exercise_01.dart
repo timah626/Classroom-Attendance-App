a876ede32d863b38f7b7c55fa3e0c80ba140b19a
timah626 <timahchelsie@gmail.com>
2026-03-06 22:45:19 +0100

    Delete exercise_01 directory

--- exercise_01/bin/exercise_01.dart
+++
@@ -1,5 +0,0 @@
-import 'package:exercise_01/exercise_01.dart' as exercise_01;
-
-void main(List<String> arguments) {
-  print('Hello world: ${exercise_01.calculate()}!');
-}
--- exercise_01/test/exercise_01_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:exercise_01/exercise_01.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
